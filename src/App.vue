740a0d284337c4889c1f6fce09bfaf7f98fe1b9c
Attar74 <m.elattar.dev@gmail.com>
2023-08-07 02:33:39 +0300

    project intiation

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import StayTuned from '@/components/StayTuned.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <StayTuned />
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
