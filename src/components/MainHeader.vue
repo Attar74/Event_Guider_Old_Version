a708eb3e9792ee6137ca5592353949759c474c60
Attar74 <m.elattar.dev@gmail.com>
2023-08-09 01:12:23 +0300

    header enhancment in small screens

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -103,43 +103,54 @@
 		<Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
 			<div class="fixed inset-0 z-10" />
 			<DialogPanel
-				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 				<div class="flex items-center justify-between">
 					<a href="#" class="-m-1.5 p-1.5">
-						<span class="sr-only">Your Company</span>
-						<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+						<span class="sr-only">Event Guiders</span>
+						<img class="h-8 w-auto" :src="Logo" alt="" />
 					</a>
 					<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
 						<span class="sr-only">Close menu</span>
-						<XMarkIcon class="h-6 w-6" aria-hidden="true" />
+						<XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
 					</button>
 				</div>
 				<div class="mt-6 flow-root">
-					<div class="-my-6 divide-y divide-gray-500/10">
+					<div class="-my-6 divide-y divide-white">
 						<div class="space-y-2 py-6">
+							<a href="#"
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Home</a>
 							<Disclosure as="div" class="-mx-3" v-slot="{ open }">
 								<DisclosureButton
-									class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-									Product
+									class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">
+									Services
 									<ChevronDownIcon :class="[open ? 'rotate-180' : '', 'h-5 w-5 flex-none']" aria-hidden="true" />
 								</DisclosureButton>
 								<DisclosurePanel class="mt-2 space-y-2">
-									<DisclosureButton v-for="item in [...products, ...callsToAction]" :key="item.name" as="a"
+									<DisclosureButton v-for="item in [...Services, ...callsToAction]" :key="item.name" as="a"
 										:href="item.href"
-										class="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+										class="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-gray-800">
+										{{ item.name }}</DisclosureButton>
+								</DisclosurePanel>
+							</Disclosure>
+							<Disclosure as="div" class="-mx-3" v-slot="{ open }">
+								<DisclosureButton
+									class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">
+									Venues
+									<ChevronDownIcon :class="[open ? 'rotate-180' : '', 'h-5 w-5 flex-none']" aria-hidden="true" />
+								</DisclosureButton>
+								<DisclosurePanel class="mt-2 space-y-2">
+									<DisclosureButton v-for="item in [...Venues, ...callsToAction]" :key="item.name" as="a"
+										:href="item.href"
+										class="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-gray-800">
 										{{ item.name }}</DisclosureButton>
 								</DisclosurePanel>
 							</Disclosure>
 							<a href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-							<a href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-							<a href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Favourite</a>
 						</div>
 						<div class="py-6">
 							<a href="#"
-								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
+								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">Log
 								in</a>
 						</div>
 					</div>
@@ -175,19 +186,28 @@ import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/vue/20/so
 import Logo from "@/assets/img/logo.png"
 
 const Services = [
-	{ name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-	{ name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-	{ name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-	{ name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-	{ name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Event Planners', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+	{ name: 'Weddings Planners', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+	{ name: 'Entertainers', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+	{ name: 'Stages&Equipments', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+	{ name: 'Makeup Artists', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Dj’s', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Musician &Djs', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Security', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Catering', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Photo / Videographers', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'HairCare', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Decoration', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+
+
 ]
 
 const Venues = [
-	{ name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-	{ name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-	{ name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-	{ name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-	{ name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+	{ name: 'Hotels', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+	{ name: 'Wedding Venues', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+	{ name: 'Boats', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+	{ name: 'Private-villas', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+	{ name: 'Restaurants', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
 ]
 
 const callsToAction = [
